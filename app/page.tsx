c75efbabb2929ab9163a5e323c5f3f1982e8255a
ibrahimi46 <anasibrahimi4664@gmail.com>
2025-11-08 13:01:19 +0300

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <div>main page</div>
-    </main>
+    <>
+      <div className="fixed inset-0 -z-100">
+        <Image
+          src={"/background_dark.jpg"}
+          fill
+          alt="background"
+          priority
+          quality={75}
+        />
+        <div className="absolute inset-0 bg-black/10" />
+      </div>
+      <main className="relative min-h-screen flex items-center justify-center">
+        <div>main page</div>
+      </main>
+    </>
   );
 }
